0468cc798ab648074f37e0e269f1e29b19e3a577
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2024-01-05 13:35:40 -0500

    Deleting unused files

--- cmake-build-debug/_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tga.c.o.d
+++
@@ -1,2 +0,0 @@
-_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tga.c.o: \
-  /Users/hugh/Desktop/jj-git-repos/ChessPP/cmake-build-debug/_deps/sdl2_image-src/IMG_tga.c
--- cmake-build-debug/_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tif.c.o.d
+++
@@ -1,2 +0,0 @@
-_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tif.c.o: \
-  /Users/hugh/Desktop/jj-git-repos/ChessPP/cmake-build-debug/_deps/sdl2_image-src/IMG_tif.c
