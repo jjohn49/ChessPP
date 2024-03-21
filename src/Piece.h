3d9c6b9cae557689cffd5afc15d3b764a8ead795
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2024-03-21 13:52:37 -0400

    Deleted old stuff

---
+++ src/Piece.h
@@ -0,0 +1,17 @@
+//
+// Created by hugh on 3/21/24.
+//
+
+#ifndef CHESSPP_PIECE_H
+#define CHESSPP_PIECE_H
+
+
+class Piece {
+protected:
+    int row;
+    int col;
+
+};
+
+
+#endif //CHESSPP_PIECE_H
