14778d08f6d5c60f2ecde547e7766d4bf289556a
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2024-03-25 20:39:08 -0400

    Pawns can now take

--- src/Sources/Pawn.cpp
+++ src/Sources/Pawn.cpp
@@ -10,6 +10,8 @@ Pawn::Pawn(int row, int col, Piece::Color color): Piece(row, col, color, Piece::
 vector<Move> Pawn::getMoves(Board *board) {
     int colorDirection = (this->getColor() == White)? 1: -1;
     vector<Move> pawnMoves{};
+
+    //Gets the non-take moves, including the double hop at the start
     if(board->getPieceAt(this->row + (1*colorDirection), this->col) == nullptr){
         pawnMoves.push_back(Move(this->row,this->col, this->row + (1 * colorDirection), this->col, shared_from_this()));
 
@@ -17,6 +19,19 @@ vector<Move> Pawn::getMoves(Board *board) {
             pawnMoves.push_back(Move(this->row,this->col, this->row + (2 * colorDirection), this->col, shared_from_this()));
         }
     }
+
+    //Gets the take moves
+    if(col > 0 && (0 < row && row < 7)){
+        if(board->getPieceAt(row + (1*colorDirection), col - 1) != nullptr && board->getPieceAt(row + (1*colorDirection), col - 1)->getColor() != getColor()){
+            pawnMoves.push_back(Move(row,col,row + (1*colorDirection),col-1,shared_from_this(), board->getPieceAt(row + (1*colorDirection), col - 1)));
+        }
+    }
+    if(col < 7&& (0 < row && row < 7)){
+        if(board->getPieceAt(row + (1*colorDirection), col + 1)!= nullptr && board->getPieceAt(row + (1*colorDirection), col + 1)->getColor() != getColor()){
+            pawnMoves.push_back(Move(row,col, row + (1*colorDirection), col + 1, shared_from_this(), board->getPieceAt(row + (1*colorDirection), col + 1)));
+        }
+    }
+
     return pawnMoves;
 }
 
